d876be57e93b28ae64eba9db959d5c6f1cbdb23b
Lars Brinkhoff <lars@nocrew.org>
2021-03-19 03:45:41 +0000

    Files extracted from 169279.tape

---
+++ jwood/modty1.cmd
@@ -0,0 +1,25 @@
+
+mod bs2310.tym
+s. 6+
+sioinc	eq	1
+. 6+nex
+mod bs2311.tym
+s. 6+
+sioinc	eq	1
+. 6+nex
+mod bs2312.tym
+s. 6+
+sioinc	eq	1
+. 6+nex
+mod bs2313.tym
+s. 6+
+sioinc	eq	1
+. 6+nex
+mod bs2314.tym
+s. 6+
+sioinc	eq	1
+. 6+nex
+mod bs2315.tym
+s. 6+
+sioinc	eq	1
+. 6+n
