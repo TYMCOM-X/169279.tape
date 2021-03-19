938615ae80456de99010687b0f361bfb7df46adb
Lars Brinkhoff <lars@nocrew.org>
2021-03-19 11:08:17 +0000

    Files extracted from 169279.tape

---
+++ wrfs/di2120.cmd
@@ -0,0 +1,21 @@
+;;*****************************************************************************
+;;              FILE:   KERNAL
+;;      COMPANY NAME:   DIALOG
+;;       NODE NUMBER:   2120                   LOCATION:   ANN ARBOR, MI
+;;       KERNAL HOST:   120
+;;       SLOT NUMBER:   FF
+;;           VERSION:   5.08                       DATE:   29-JAN-85
+;;          INITIALS:   LDB
+;;*****************************************************************************
+;;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>REVISION HISTORY<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+;;   DI2120.CMD INITIAL CONFIGURATION.   MSK   4/4/84
+;;*****************************************************************************
+;@(WRFSCODE)I2IS05.I08
+;ADI2120.TYM
+;AGOODGY.LST
+;@(WRFSCODE)I2IS05.R08
+;@(WRFSCODE)I2T205.R08
+;@(WRFSCODE)I2IG05.R08
+1;FDI2120.NIB
+70;T
+0,IEND;P%PSEGF,SFSIZE;P%Q
