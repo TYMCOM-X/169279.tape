e04ac5388972c90b78e54c6f7a62dceb73c70cb6
Lars Brinkhoff <lars@nocrew.org>
2021-03-19 10:16:32 +0000

    Files extracted from 169279.tape

---
+++ uksup/testsc.cmd
@@ -0,0 +1,25 @@
+;;*********************************************************************
+;;          File:  Solo Consat Command
+;;  Company Name:  West Yorkshire Police
+;;   Node Number:  2015
+;;   KerneL Host:  015
+;;   Slot Number:  00
+;;       Version:  5.11
+;;      Initials:  CFH
+;;*********************************************************************
+;A(SOURCE)ISCS03.I05
+;A(SOURCE)TII05.I11
+;A(gharper)WY2015.TYM
+;A(WYNET)SOLOGG.WYP
+;A(SOURCE)TII05.R11
+;A(SOURCE)ISCS03.R05
+;A(SOURCE)TII05.F11
+;A(SOURCE)TII05.P11
+;X(SOURCE)ISCS03.P05
+1;FWY2015.BND
+70;TPRIVBG,RANGE1;PSEG1BG,SEG1RL,SEG1SZ;PXRAYBG,XRAYRL,XRAYSZ;P%P%Q
+;;--------------------------------------------------------------------:
+;;Change History - Date - Initials - Change Made
+;;              15/Jan/87 - CFH - Initial Configuration
+;;
+;;
