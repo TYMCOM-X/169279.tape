d7fdaef990a13dea7a01543d12edabababf28ad7
Lars Brinkhoff <lars@nocrew.org>
2021-03-19 07:59:02 +0000

    Files extracted from 169279.tape

---
+++ tkochmann/lsti2s.cmd
@@ -0,0 +1,16 @@
+4,isis2;(
+;; ISIS-II Version 7.03 Sample Command File
+ LIST F,-X,-R
+ Title ISIS Version 07.03 - Marie Doyle
+132,64;%
+;@(source)I2IS07.I03
+;@(NTS)tk2770.TYM
+;@(NTS)GOODGY.nts
+;;@(source)I2IS07.D03
+;;@(source)I2IS07.S03
+;@(source)I2IS07.R03
+;X(source)I2IS07.P03
+;)
+;; on the "-" prompt enter "cross"
+;;
+%q
