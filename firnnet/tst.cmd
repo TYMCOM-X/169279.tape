d3d5063fbf1d3d62756920b9818973bf53def587
Lars Brinkhoff <lars@nocrew.org>
2021-03-19 03:06:02 +0000

    Files extracted from 169279.tape

---
+++ firnnet/tst.cmd
@@ -0,0 +1,25 @@
+;; **********************************************************************
+;; *                                                                    *
+;; *  File     : FP2100.TYM        Customer : CENTEL                    *
+;; *  Slot     : FF/00             Site     : 132 Calhoun Street        *
+;; *                               Location : 4th Floor                 *
+;; *                                          Tallahassee, FL 32301     *
+;; *                               Site     : 904/681-2679              *
+;; *  Node     : 2100              Contact  : William Price             *
+;; *  Kernel   : 300               Phone    : 904/599-1769              *
+;; *  Machine  : HDLC MICRO                                             *
+;; *  Core     : 1024kb                                                 *
+;; *                                                                    *
+;; **********************************************************************
+
+
+;@(SOURCE)I2IS07.I03
+;@tst.TYM
+;@STATS.NAD
+;@ISIS.GGY
+;@(SOURCE)I2IS07.D03
+;@(SOURCE)I2IS07.S03
+;@(SOURCE)I2IS07.R03
+;X(SOURCE)I2IS07.P03
+1;Ftst.NIB
+70;T0,SUMTBL;PINTLEN,IEND-INTLEN;P%PSEGF,SFSIZE;P%Q
