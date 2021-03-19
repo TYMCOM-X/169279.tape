f35b26f07e97c9543fad0ec45e916dfe08307fc4
Lars Brinkhoff <lars@nocrew.org>
2021-03-19 10:27:35 +0000

    Files extracted from 169279.tape

---
+++ ul6net/jim.cmd
@@ -0,0 +1,21 @@
+;;......................................................................
+;;.                                                                    .
+;;.                      Global Data Network II                        .
+;;.                                                                    .
+;;.                            Kernel File                             .
+;;.                                                                    .
+;;.        NODE: 2227                            KERNEL: 227           .
+;;.       HOSTS: 227                               SLOT: 0             .
+;;. APPLICATION: ISIS KERNAL                    VERSION: 7.03          .
+;;.     CREATED: 10-OCT-90                       AUTHOR: AJF           .
+;;.LAST REVISED:                                REVISOR:               .
+;;.                                                                    .
+;;......................................................................
+;; T-II/ISIS NODE CODE COMMAND FILE
+;@(SOURCE)I2IS07.I03
+;@JIM.TYM
+;@GOODGY.ZZZ
+;@(SOURCE)I2IS07.R03
+;X(SOURCE)I2IS07.P03
+1;FJIM.NIB
+70;T0,SUMTBL;PINTLEN,IEND-INTLEN;P%PSEGF,SFSIZE;P%Q
